d0f76beb85b0c96390c400b2554991c938ac3301
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-01 22:00:15 +0600

    dynamic problem solve

--- js/app.js
+++ js/app.js
@@ -13,10 +13,29 @@ const searchMobile = async () => {
     // console.log(url);
     const res = await fetch(url);
     const data = await res.json();
-    displaySearchResult(data.data[0]);
+    displaySearchResult(data.data);
 }
 
 //display search result
-const displaySearchResult = mobile => {
-    console.log(mobile);
+const displaySearchResult = mobiles => {
+    // console.log(mobiles);
+    const searchResult = document.getElementById('search-result');
+    // //clear all search result data
+    // // searchResult.textContent = '';
+
+    mobiles.forEach(mobile => {
+        console.log(mobile);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card">
+            <img src="${mobile.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center">${mobile.phone_name}</h5>
+                    <h6 class="card-title text-center">${mobile.brand}</h6>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    });
 }
