b4e9e44d1e524fc055b8a7981c9bb08db05bd28f
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-02 02:08:02 +0600

    error handing

--- js/app.js
+++ js/app.js
@@ -6,14 +6,19 @@ const searchMobile = async () => {
   //clear search field value
   searchField.value = "";
 
-  //set the url with https and make it *dynamic
-  //and load the url by async await
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  //error handling
+  if (searchText == "") {
+    alert("please write something in search field");
+  } else {
+    //set the url with https and make it *dynamic
+    //and load the url by async await
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
-  // console.log(url);
-  const res = await fetch(url);
-  const data = await res.json();
-  displaySearchResult(data.data);
+    // console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    displaySearchResult(data.data);
+  }
 };
 
 //display search result
@@ -51,14 +56,14 @@ const loadMobileDetail = async (id) => {
   displayMobileDetail(data.data);
 };
 
-const displayMobileDetail = mobile => {
-    console.log(mobile);
-    const mobileDetails = document.getElementById('mobile-details');
-    //clear all result
-    mobileDetails.textContent = '';
-    const div = document.createElement('div');
-    div.classList.add('card');
-    div.innerHTML = `
+const displayMobileDetail = (mobile) => {
+  console.log(mobile);
+  const mobileDetails = document.getElementById("mobile-details");
+  //clear all result
+  mobileDetails.textContent = "";
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = `
     <div class="row g-0 single-card-style"> 
         <h5 class="card-title mobile-name-custom-bg">${mobile.name}</h5>
         <div class="col-md-4">
@@ -67,15 +72,17 @@ const displayMobileDetail = mobile => {
         <div class="col-md-8">
             <div class="card-body">
                 <p class="card-text">${mobile.releaseDate}</p>
+                <p class="card-text">${mobile.mainFeatures.displaySize}</p>
                 <p class="card-text">${mobile.mainFeatures.storage}</p>
                 <p class="card-text">${mobile.mainFeatures.chipSet}</p>
                 <p class="card-text">${mobile.mainFeatures.memory}</p>
-                <p class="card-text text-white">Others: ${mobile.others.WLAN}, ${mobile.others.Bluetooth}, ${mobile.others.GPS}, ${mobile.others.USB}</p>
-                <p class="card-text text-dark text-bold text-uppercase">${mobile.brand}</p>
-                <p class="card-text text-primary"><small class="text-muted">${mobile.slug}</small></p>
+                <p class="card-text text-white">Sensors: ${mobile.mainFeatures.sensors[0]}, ${mobile.mainFeatures.sensors[1]}, ${mobile.mainFeatures.sensors[2]}, ${mobile.mainFeatures.sensors[3]}, ${mobile.mainFeatures.sensors[4]}, ${mobile.mainFeatures.sensors[5]}, ${mobile.mainFeatures.sensors[6]}.</p>
+                <p class="card-text text-white">Others: Bluetooth: ${mobile.others.Bluetooth}, GPS: ${mobile.others.GPS}, NFC:${mobile.others.NFC}, Radio: ${mobile.others.Radio}, USB: ${mobile.others.USB}, WLAN: ${mobile.others.WLAN}.</p>
+                <p class="card-text text-dark fw-bold text-uppercase">${mobile.brand}</p>
+                <p class="card-text text-primary"><small class="text-muted">Tags: ${mobile.slug}</small></p>
             </div>
         </div>
     </div>
     `;
-    mobileDetails.appendChild(div);
-}
+  mobileDetails.appendChild(div);
+};
