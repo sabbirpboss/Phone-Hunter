0016ada5792cbd0c1ecf7850a4cb75559dc7ac18
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-02 13:03:08 +0600

    some changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.getElementById('error').style.display = "none";
+document.getElementById("error").style.display = "none";
 
 const searchMobile = async () => {
   // call id and get input value
@@ -8,44 +8,35 @@ const searchMobile = async () => {
   //clear search field value
   searchField.value = "";
 
-  //error handling
-  // if (searchText == "") {
-  //   alert("please write something in search field");
-  // } 
-  // else {
-    //set the url with https and make it *dynamic
-    //and load the url by async await
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
-    // console.log(url);
-    const res = await fetch(url);
-    const data = await res.json();
-    displaySearchResult(data.data);
-  // }
+  // console.log(url);
+  const res = await fetch(url);
+  const data = await res.json();
+  displaySearchResult(data.data);
 };
 
 //display search result
 const displaySearchResult = (mobiles) => {
-  console.log(mobiles);
+  // console.log(mobiles);
   if (mobiles.length == 0) {
-    document.getElementById('error').style.display = "block";
-    document.getElementById('search-result').innerHTML = '';
-    document.getElementById('mobile-details').innerHTML = '';
-  }
-  else{
-    const something = mobiles.slice(0, 20);
-  
-  const searchResult = document.getElementById("search-result");
-  document.getElementById('error').style.display ='none';
-  document.getElementById('search-field').value = '';
-  //clear all search result data
-  searchResult.textContent = "";
-  
-      something.forEach((mobile) => {
-    // console.log(mobile);
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = `
+    document.getElementById("error").style.display = "block";
+    document.getElementById("search-result").innerHTML = "";
+    document.getElementById("mobile-details").innerHTML = "";
+  } else {
+    const searchResultSlice = mobiles.slice(0, 20);
+
+    const searchResult = document.getElementById("search-result");
+    document.getElementById("error").style.display = "none";
+    document.getElementById("search-field").value = "";
+    //clear all search result data
+    searchResult.textContent = "";
+
+    searchResultSlice.forEach((mobile) => {
+      // console.log(mobile);
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
             <div id="card-custom-bg" class="card text-white">
                 <img id="search-result-image" src="${mobile.image}" class="card-img-top" alt="...">
                 <div class="card-body card-body-color">
@@ -55,11 +46,9 @@ const displaySearchResult = (mobiles) => {
                 </div>
             </div>
         `;
-    searchResult.appendChild(div);
-  });
-
-};
-
+      searchResult.appendChild(div);
+    });
+  }
 };
 
 //get mobile detail by mobile id
@@ -83,19 +72,41 @@ const displayMobileDetail = (mobile) => {
     <div class="row g-0 single-card-style"> 
         <h5 class="card-title mobile-name-custom-bg">${mobile.name}</h5>
         <div class="col-md-4">
-            <img src="${mobile.image}" class="img-fluid single-item-image rounded-start" alt="...">
+            <img src="${
+              mobile.image
+            }" class="img-fluid single-item-image rounded-start" alt="...">
         </div>
         <div class="col-md-8">
             <div class="card-body">
-                <p class="card-text">${mobile.releaseDate ? mobile.releaseDate:'comming soon'}</p>
+                <p class="card-text">${
+                  mobile.releaseDate ? mobile.releaseDate : "comming soon"
+                }</p>
                 <p class="card-text">${mobile.mainFeatures.displaySize}</p>
                 <p class="card-text">${mobile.mainFeatures.storage}</p>
                 <p class="card-text">${mobile.mainFeatures.chipSet}</p>
                 <p class="card-text">${mobile.mainFeatures.memory}</p>
-                <p class="card-text text-white">Sensors: ${mobile.mainFeatures.sensors[0]}, ${mobile.mainFeatures.sensors[1]}, ${mobile.mainFeatures.sensors[2]}, ${mobile.mainFeatures.sensors[3]}, ${mobile.mainFeatures.sensors[4]}, ${mobile.mainFeatures.sensors[5]}, ${mobile.mainFeatures.sensors[6]}.</p>
-                <p class="card-text text-white">Others: Bluetooth: ${mobile.others ? mobile.others.Bluetooth:'Not Yet Found'}, GPS: ${mobile.others ? mobile.others.GPS:'Not Yet Found'}, NFC:${mobile.others ? mobile.others.NFC: 'Not Yet Found'}, Radio: ${mobile.others ? mobile.others.Radio: 'Not Yet Found'}, USB: ${mobile.others ? mobile.others.USB: 'Not Yet Found'}, WLAN: ${mobile.others ? mobile.others.WLAN: 'Not Yet Found'}.</p>
-                <p class="card-text text-dark fw-bold text-uppercase">${mobile.brand}</p>
-                <p class="card-text text-primary"><small class="text-muted">Tags: ${mobile.slug}</small></p>
+                <p class="card-text text-white">Sensors: ${
+                  mobile.mainFeatures.sensors[0]
+                }, ${mobile.mainFeatures.sensors[1]}, ${
+    mobile.mainFeatures.sensors[2]
+  }, ${mobile.mainFeatures.sensors[3]}, ${mobile.mainFeatures.sensors[4]}, ${
+    mobile.mainFeatures.sensors[5]
+  }, ${mobile.mainFeatures.sensors[6]}.</p>
+                <p class="card-text text-white">Others: Bluetooth: ${
+                  mobile.others ? mobile.others.Bluetooth : "Not Yet Found"
+                }, GPS: ${
+    mobile.others ? mobile.others.GPS : "Not Yet Found"
+  }, NFC:${mobile.others ? mobile.others.NFC : "Not Yet Found"}, Radio: ${
+    mobile.others ? mobile.others.Radio : "Not Yet Found"
+  }, USB: ${mobile.others ? mobile.others.USB : "Not Yet Found"}, WLAN: ${
+    mobile.others ? mobile.others.WLAN : "Not Yet Found"
+  }.</p>
+                <p class="card-text text-dark fw-bold text-uppercase">${
+                  mobile.brand
+                }</p>
+                <p class="card-text text-primary"><small class="text-muted">Tags: ${
+                  mobile.slug
+                }</small></p>
             </div>
         </div>
     </div>
@@ -103,11 +114,7 @@ const displayMobileDetail = (mobile) => {
   mobileDetails.appendChild(div);
 };
 
-
-
-
-
-  document.getElementById('button-search').addEventListener('click', function (){
-    const mobileDetails = document.getElementById("mobile-details");
-    mobileDetails.textContent = '';
-  })
+document.getElementById("button-search").addEventListener("click", function () {
+  const mobileDetails = document.getElementById("mobile-details");
+  mobileDetails.textContent = "";
+});
